11404a25d5506e5cf73e5714e6a45dc5a45c64b2
Kevin Bi <keveloper@gmail.com>
2023-05-31 19:58:13 -0400

    Check if there is only a small percentage difference

--- scripts/check-textbook.py
+++ scripts/check-textbook.py
@@ -4,7 +4,10 @@ import os
 textbook_size = os.stat("textbook.pdf").st_size
 main_size = os.stat("main.pdf").st_size
 
-print("textbook.pdf:", textbook_size)
-print("main.pdf:", main_size)
+print("textbook.pdf: ", textbook_size)
+print("main.pdf: ", main_size)
 
-sys.exit(textbook_size != main_size) 
+
+diff_pct = abs(textbook_size - main_size) / textbook_size
+print("diff_pct: ", diff_pct)
+sys.exit(diff_pct > 2) 
